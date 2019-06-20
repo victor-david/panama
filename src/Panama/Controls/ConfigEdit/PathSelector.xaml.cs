75b34a52139db2004563c2aac46ed196ddbd5250
Victor D. Sandiego <codepuppy@balticsoap.com>
2019-06-20 11:27:41 -0500

    Comment out deprecated mapi call.

--- src/Panama/Controls/ConfigEdit/PathSelector.xaml.cs
+++ src/Panama/Controls/ConfigEdit/PathSelector.xaml.cs
@@ -136,7 +136,7 @@ namespace Restless.App.Panama.Controls
                     SelectFileSystem();
                     break;
                 case PathSelectorType.Mapi:
-                    SelectMapiFolder();
+                    //SelectMapiFolder();
                     break;
             }
         }
