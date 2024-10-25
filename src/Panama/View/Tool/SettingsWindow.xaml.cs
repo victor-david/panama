f4d7b6f9daf6f206dff79e13abbcb0f7aa583c39
Victor D. Sandiego <codepuppy@balticsoap.com>
2024-10-25 07:58:03 -0600

    Settings window (basics)

--- src/Panama/View/Tool/SettingsWindow.xaml.cs
+++ src/Panama/View/Tool/SettingsWindow.xaml.cs
@@ -1,11 +1,11 @@
-﻿using Restless.Toolkit.Controls;
+﻿using MahApps.Metro.Controls;
 
 namespace Restless.Panama.View
 {
     /// <summary>
     /// Interaction logic for SettingsWindow.xaml
     /// </summary>
-    public partial class SettingsWindow : AppWindow
+    public partial class SettingsWindow : MetroWindow
     {
         public SettingsWindow()
         {
