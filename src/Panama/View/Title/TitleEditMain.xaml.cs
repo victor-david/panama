e978f82d091f3a528c54b39a8809a68f8acb78d8
Victor D. Sandiego <codepuppy@balticsoap.com>
2024-10-27 12:50:18 -0600

    Update main title data / styles

--- src/Panama/View/Title/TitleEditMain.xaml.cs
+++ src/Panama/View/Title/TitleEditMain.xaml.cs
@@ -5,7 +5,7 @@ namespace Restless.Panama.View
     /// <summary>
     /// Interaction logic for TitleEditMain.xaml
     /// </summary>
-    public partial class TitleEditMain : StackPanel
+    public partial class TitleEditMain : Grid
     {
         public TitleEditMain()
         {
