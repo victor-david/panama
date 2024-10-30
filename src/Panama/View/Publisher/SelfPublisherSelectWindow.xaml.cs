4876489ded4a884d428119c814af5960b25fe59c
Victor D. Sandiego <codepuppy@balticsoap.com>
2024-10-29 19:23:33 -0600

    Update title / publisher select
    Update the windows for selecting a publisher or self publisher for a title

--- src/Panama/View/Publisher/SelfPublisherSelectWindow.xaml.cs
+++ src/Panama/View/Publisher/SelfPublisherSelectWindow.xaml.cs
@@ -4,13 +4,13 @@
  * Panama is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License v3.0
  * Panama is distributed in the hope that it will be useful, but without warranty of any kind.
 */
+using MahApps.Metro.Controls;
 using Restless.Panama.Database.Tables;
 using Restless.Panama.ViewModel;
-using Restless.Toolkit.Controls;
 
 namespace Restless.Panama.View
 {
-    public partial class SelfPublisherSelectWindow : AppWindow
+    public partial class SelfPublisherSelectWindow : MetroWindow
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="SelfPublisherSelectWindow"/> class
