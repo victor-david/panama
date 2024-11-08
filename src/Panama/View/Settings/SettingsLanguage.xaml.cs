41fdd79b40c7e077c503d1695bf135d99ce7589b
Victor D. Sandiego <codepuppy@balticsoap.com>
2024-11-08 15:55:09 -0600

    Language settings

---
+++ src/Panama/View/Settings/SettingsLanguage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Restless.Panama.View
+{
+    /// <summary>
+    /// Interaction logic for SettingLanguage.xaml
+    /// </summary>
+    public partial class SettingsLanguage : UserControl
+    {
+        public SettingsLanguage()
+        {
+            InitializeComponent();
+        }
+    }
+}
