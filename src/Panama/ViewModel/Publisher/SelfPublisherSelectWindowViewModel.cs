6b35f216e5b921c12f5a611eeeddfa86044047b6
Victor D. Sandiego <codepuppy@balticsoap.com>
2024-11-14 10:02:44 -0600

    Update SelfPublisherSelectWindowViewModel.cs
    Remove secondary sort, cleanup

--- src/Panama/ViewModel/Publisher/SelfPublisherSelectWindowViewModel.cs
+++ src/Panama/ViewModel/Publisher/SelfPublisherSelectWindowViewModel.cs
@@ -1,9 +1,3 @@
-/*
- * Copyright 2019 Victor D. Sandiego
- * This file is part of Panama.
- * Panama is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License v3.0
- * Panama is distributed in the hope that it will be useful, but without warranty of any kind.
-*/
 using Restless.Panama.Core;
 using Restless.Panama.Database.Tables;
 using Restless.Panama.Resources;
@@ -65,8 +59,7 @@ namespace Restless.Panama.ViewModel
                 .MakeInitialSortDescending();
 
             Columns.Create(Header.PublishedCountShort, TableColumns.Calculated.PubCount)
-                .MakeFixedWidth(FixedWidth.W052)
-                .AddSort(null, TableColumns.Name, DataGridColumnSortBehavior.AlwaysAscending);
+                .MakeFixedWidth(FixedWidth.W052);
 
             Commands.Add("Select", RunSelectCommand, p => IsSelectedRowAccessible);
         }
