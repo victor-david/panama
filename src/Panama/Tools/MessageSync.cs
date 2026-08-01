e0c94f9da0ec1b3e895a759e8d28b4e74e06e79e
Victor D. Sandiego <codepuppy@balticsoap.com>
2026-08-01 15:38:58 -0600

    Update submission message scan
    When scanning submission messages, check sub directories and adjust the database entry. This allows the user to organize messages (by year, for example) in sub directories. Once re-organized, the message scan will find the new locations. You still need to place new messages in the root of the message directory in order to add new messages to a submission. That's where the message-add functionality looks. By placing older messages in sub directories, the message-add is faster since it's creating mime messages from fewer files.

--- src/Panama/Tools/MessageSync.cs
+++ src/Panama/Tools/MessageSync.cs
@@ -17,35 +17,49 @@ namespace Restless.Panama.Tools
         {
             FileScanResult result = new();
 
+            // empty. only init if needed.
+            string[] allFiles = Array.Empty<string>();
+
             foreach (SubmissionMessageRow message in SubmissionMessageTable.EnumerateFileSystem())
             {
                 result.ScanCount++;
-                string fileName = Path.Combine(Config.Instance.FolderSubmissionMessage, message.EntryId);
 
-                MimeKitMessage msg = new(fileName);
+                string fullPath = Paths.SubmissionMessage.WithRoot(message.EntryId);
+
+                if (!File.Exists(fullPath))
+                {
+                    if (allFiles.Length == 0)
+                    {
+                        allFiles = Directory.GetFiles(Config.Instance.FolderSubmissionMessage, "*", SearchOption.AllDirectories);
+                    }
+                    fullPath = FindMissingMessageFile(allFiles, fullPath, Path.GetFileName(message.EntryId));
+                }
+
+                MimeKitMessage msg = new(fullPath);
                 if (!msg.IsError)
                 {
-                    string newFileName = GetSynchronizedFileName(msg, fileName);
-                    if (message.EntryId != newFileName)
+                    string newEntryId = GetSynchronizedEntryId(msg, fullPath);
+
+                    if (message.EntryId != newEntryId)
                     {
                         try
                         {
-                            string newFileNameFull = Path.Combine(Config.Instance.FolderSubmissionMessage, newFileName);
-                            File.Move(fileName, newFileNameFull);
-                            message.UpdateEntryId(newFileName);
-                            result.AppendOutputText($"Update {message.EntryId} to {newFileName}");
-                            result.Updated.Add(FileScanItem.Create(newFileName));
+                            string newFullPath = Paths.SubmissionMessage.WithRoot(newEntryId);
+                            File.Move(fullPath, newFullPath);
+                            message.UpdateEntryId(newEntryId);
+                            result.AppendOutputText($"Update {message.EntryId} to {newEntryId}");
+                            result.Updated.Add(FileScanItem.Create(newEntryId));
                         }
                         catch (Exception ex)
                         {
                             result.AppendOutputText(ex.Message);
                         }
-
                     }
                 }
                 else
                 {
-                    result.NotFound.Add(FileScanItem.Create(fileName));
+                    result.AppendOutputText("Not found: " + fullPath);
+                    result.NotFound.Add(FileScanItem.Create(fullPath));
                 }
             }
 
@@ -53,19 +67,43 @@ namespace Restless.Panama.Tools
             return result;
         }
 
-        private string GetSynchronizedFileName(MimeKitMessage msg, string fileName)
+        private string FindMissingMessageFile(string[] allFiles, string currentFullPath, string entryId)
         {
+            foreach (string fullPath in allFiles)
+            {
+                if (Path.GetFileName(fullPath) == entryId)
+                {
+                    return fullPath;
+                }
+            }
+            return currentFullPath;
+        }
+
+        private string GetSynchronizedEntryId(MimeKitMessage msg, string fullPath)
+        {
+            string entryId = Paths.SubmissionMessage.WithoutRoot(fullPath);
+            string directoryPreface = string.Empty;
+
+            // see if message file is inside a sub directory
+            if (entryId.Contains(Path.DirectorySeparatorChar))
+            {
+                directoryPreface = Path.GetDirectoryName(entryId) + Path.DirectorySeparatorChar;
+            }
+
             string cleanSubject = GetCleanStr(msg.Subject);
             string cleanSender = GetCleanStr(msg.FromName, msg.FromName == msg.FromEmail);
 
             // MessageDate_Subject.ext
             // 2022-07-28_17.21.09_Subject.eml
-            return
+
+            string synchronizedEntryId =
                 string.Format(CultureInfo.InvariantCulture, "{0}_{1}_{2}{3}",
                 msg.MessageDateUtc.ToString("yyyy-MM-dd_HH.mm.ss", CultureInfo.InvariantCulture),
                 Format.ValidFileName(cleanSender),
                 Format.ValidFileName(cleanSubject),
-                Path.GetExtension(fileName));
+                Path.GetExtension(entryId));
+
+            return directoryPreface + synchronizedEntryId;
         }
 
         private string GetCleanStr(string str, bool allowDot = false)
